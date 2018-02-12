b9e3add84c85558200b2f4a4252e3d43346cb0cf
snakeex <mikidosa@gmail.com>
2018-02-12 22:14:01 +0100

    form, panel elemek kinézet; roboto betűtípus

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,19 +1,31 @@
 <div class="container loginform">
   <div class="row">
-    <div class="col-md-4 col-md-offset-4">
-      <form action="login" method="POST">
-        <div class="form-group">
-          <label>Felhasználónév</label>
-          <input type="text" class="form-control" name="username">
-        </div>
-        <div class="form-group">
-          <label>Jelszó</label>
-          <input type="password" class="form-control" name="password">
-        </div>
-        <div class="text-center">
-         <button type="submit" class="btn btn-default">Belépés</button>
-        </div>
-      </form>
+    <div class="col-md-6 col-md-offset-3">
+	  <div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3>Bejelentkezés</h3>
+		</div>
+		<div class="panel-body">
+			<form action="login" method="POST">
+				<fieldset>
+					<div class="form-group">
+					  <label>Felhasználónév</label>
+					  <input type="text" class="form-control" name="username">
+					</div>
+					<div class="form-group">
+					  <label>Jelszó</label>
+					  <input type="password" class="form-control" name="password">
+					</div>
+				</fieldset>
+				<div>
+				 <button class="btn btn-primary btn-lg" type="submit" class="btn btn-default">Belépés</button>
+				</div>
+			</form>
+		</div>
+		<div class="panel-footer">
+			<a href="#">Elfelejtett jelszó</a>
+		</div>
+	  </div>
     </div>
   </div>
 </div>
--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -4,6 +4,7 @@
     <link href="<?php echo base_url(); ?>assets/css/bootstrap-theme.min.css" rel="stylesheet" />
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" />
     <link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
     <link rel="icon" href="<?php echo base_url(); ?>assets/imgs/favicon.png" type="image/x-icon" />
   </head>
   <body>
